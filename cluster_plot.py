4e87296037b49936925e1a402a92d201ce283b0b
tom-burdge <tomburdge@wessexinsights.com>
2023-10-31 17:42:32 +0000

    refactor clustering to a metaflow flow

---
+++ cluster_plot.py
@@ -0,0 +1,39 @@
+import os
+
+import altair as alt
+from metaflow import Flow
+from ulid import ULID
+
+run = Flow("ClusterTrainFlow").latest_run
+
+plot_df = run["assign_labels"].task.data.plot_df
+
+chart1 = (
+    alt.Chart(plot_df)
+    .mark_circle(size=60)
+    .encode(
+        x="component_2",
+        y="component_1",
+        color="labels",
+        tooltip=["volume_name", "labels", "basket"],
+    )
+    .interactive()
+)
+
+chart2 = (
+    alt.Chart(plot_df)
+    .mark_circle(size=60)
+    .encode(
+        x="component_2",
+        y="component_1",
+        color="basket",
+        tooltip=["volume_name", "labels", "basket"],
+    )
+    .interactive()
+)
+
+combined_chart = alt.vconcat(chart1, chart2)
+
+combined_chart.save(
+    os.path.join("visualisations", "kmeans", f"{str(ULID())}-chart.html")
+)
---
+++ cluster_train.py
@@ -0,0 +1,88 @@
+from metaflow import FlowSpec, step
+
+
+class ClusterTrainFlow(FlowSpec):
+    @step
+    def start(self):
+        from src import DuckbClient
+
+        client = DuckbClient()
+        self.df = client.execute_sql_string("SELECT * FROM tipitaka_preprocessed").pl()
+
+        self.next(self.exclude)
+
+    @step
+    def exclude(self):
+        from polars import exclude
+
+        self.data = self.df.select(exclude(["volume_name", "basket"])).to_numpy()
+
+        self.next(self.transform, self.reduce_dimensions)
+
+    @step
+    def transform(self):
+        from sklearn.decomposition import PCA
+
+        # from sklearn.manifold import TSNE
+        from sklearn.pipeline import Pipeline
+        from sklearn.preprocessing import StandardScaler
+
+        transformer_pipeline = Pipeline(
+            [
+                ("scaler", StandardScaler()),
+                ("pca", PCA(n_components=0.85)),  # PCA retaining 85% of variance
+                # ("tsne", TSNE(3)),
+            ]
+        )
+
+        self.X = transformer_pipeline.fit_transform(self.data)
+        self.next(self.train)
+
+    @step
+    def train(self):
+        from sklearn.cluster import HDBSCAN
+
+        model = HDBSCAN(min_cluster_size=2)
+        self.labels = model.fit_predict(self.X)
+        self.next(self.assign_labels)
+
+    @step
+    def reduce_dimensions(self):
+        from sklearn.manifold import TSNE
+        from sklearn.pipeline import Pipeline
+        from sklearn.preprocessing import StandardScaler
+
+        two_components_pipeline = Pipeline(
+            [("scaler", StandardScaler()), ("tsne", TSNE())]
+        )
+
+        self.two_components = two_components_pipeline.fit_transform(self.data)
+
+        self.next(self.assign_labels)
+
+    @step
+    def assign_labels(self, inputs):
+        from polars import DataFrame, Float64, Series
+
+        self.merge_artifacts(inputs, include=["df"])
+
+        result = self.df.with_columns(
+            DataFrame(
+                inputs.reduce_dimensions.two_components,
+                schema={"component_1": Float64, "component_2": Float64},
+            )
+        ).with_columns(Series(inputs.train.labels).alias("labels"))
+
+        self.plot_df = result.select(
+            ["component_2", "component_1", "labels", "volume_name", "basket"]
+        ).to_pandas()
+
+        self.next(self.end)
+
+    @step
+    def end(self):
+        pass
+
+
+if __name__ == "__main__":
+    ClusterTrainFlow()
